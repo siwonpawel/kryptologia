3dd5b463dc07574beae7533cbb06906813800338
Paweł Siwoń <siwonpawel@gmail.com>
2023-11-17 17:06:12 +0100

    corrects benchmark

--- src/test/java/com/zut/lab03/EncryptorBenchmark.java
+++ src/test/java/com/zut/lab03/EncryptorBenchmark.java
@@ -1,6 +1,7 @@
 package com.zut.lab03;
 
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -16,14 +17,6 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import com.zut.lab03.key.KeyPair;
 import com.zut.lab03.prime.PrimeGenerator;
-
-/*
-
-
-
-
-
- */
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
@@ -47,10 +40,15 @@ public class EncryptorBenchmark
         PrimeGenerator pg4096 = new PrimeGenerator(PrimeGenerator.KEY_SIZE_4096, random);
         PrimeGenerator pg7680 = new PrimeGenerator(PrimeGenerator.KEY_SIZE_7680, random);
 
-        e2048 = new Encryptor(new KeyPair(pg2048.generate(), pg2048.generate()));
-        e3072 = new Encryptor(new KeyPair(pg3072.generate(), pg3072.generate()));
-        e4096 = new Encryptor(new KeyPair(pg4096.generate(), pg4096.generate()));
-        e7680 = new Encryptor(new KeyPair(pg7680.generate(), pg7680.generate()));
+        KeyPair kp2048 = new KeyPair(pg2048.generate(), pg2048.generate());
+        KeyPair kp3078 = new KeyPair(pg3072.generate(), pg3072.generate());
+        KeyPair kp4096 = new KeyPair(pg4096.generate(), pg4096.generate());
+        KeyPair kp7680 = new KeyPair(pg7680.generate(), pg7680.generate());
+
+        e2048 = new Encryptor(kp2048);
+        e3072 = new Encryptor(kp3078);
+        e4096 = new Encryptor(kp4096);
+        e7680 = new Encryptor(kp7680);
     }
 
     public static void main(String[] args) throws RunnerException
@@ -67,25 +65,37 @@ public class EncryptorBenchmark
     @Benchmark
     public void test2048()
     {
-        e2048.doFinal(MESSAGE);
+        byte[] bytes = e2048.doFinal(MESSAGE);
+        byte[] bytes1 = e2048.doFinal(MESSAGE);
+
+        Arrays.equals(bytes1, bytes);
     }
 
     @Benchmark
     public void test3072()
     {
-        e3072.doFinal(MESSAGE);
+        byte[] bytes = e3072.doFinal(MESSAGE);
+        byte[] bytes1 = e3072.doFinal(bytes);
+
+        Arrays.equals(bytes1, bytes);
     }
 
     @Benchmark
     public void test4096()
     {
-        e4096.doFinal(MESSAGE);
+        byte[] bytes = e4096.doFinal(MESSAGE);
+        byte[] bytes1 = e4096.doFinal(bytes);
+
+        Arrays.equals(bytes1, bytes);
     }
 
     @Benchmark
     public void test7680()
     {
-        e7680.doFinal(MESSAGE);
+        byte[] bytes = e7680.doFinal(MESSAGE);
+        byte[] bytes1 = e7680.doFinal(bytes);
+
+        Arrays.equals(bytes1, bytes);
     }
 
 }
